04fa7018cb563f885c5e87bedb6ae752690fb4ce
mg77-cpu <hudymac29@gmail.com>
2025-01-11 01:04:16 +0200

    updated ui files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Navbar, Welcome, Footer, Loader, Transactions, Services } from './components';
+import cb from "../images/cb.mp4";
 const App = () => {
 
   return (
@@ -10,7 +11,7 @@ const App = () => {
         loop
         muted
       >
-        <source src="../images/cb.mp4" type="video/mp4" />
+        <source src= {cb} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="relative z-10 flex flex-col w-full h-screen justify-start items-center overflow-auto">
