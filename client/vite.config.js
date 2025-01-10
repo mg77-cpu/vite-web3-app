5db92264a7a70b4f958e08292f0717492280e656
mg77-cpu <hudymac29@gmail.com>
2025-01-11 00:50:28 +0200

    updated ui files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd'],
+          // Add other libraries or components you want to split into separate chunks
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit if needed
+  }
+});
