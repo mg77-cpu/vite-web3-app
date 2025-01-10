a6d79236ef7846dc59d714407bd1e91fe654eb6b
mg77-cpu <hudymac29@gmail.com>
2025-01-10 22:38:47 +0200

    changed video background file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         loop
         muted
       >
-        <source src="../images/lines.webm" type="video/mp4/webm" />
+        <source src="../images/lines.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
       <div className="relative z-10 flex flex-col w-full h-screen justify-start items-center overflow-auto">
